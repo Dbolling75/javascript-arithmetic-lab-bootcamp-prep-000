589fd49849c042e1123e1bbe2f1125b20928baca
Damia Bolling <dbolling75@gmail.com>
2018-09-18 19:03:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var a = 3;
+a = 3;
+var b = 4;
+b = 4;
+
+function add(a, b) {
+  return a + b;
+}
+  
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function inc(a) {
+return ++a;
+}
+
+function dec(a) {
+  return --a;
+}
